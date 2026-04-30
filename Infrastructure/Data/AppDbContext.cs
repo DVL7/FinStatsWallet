6a6e9e886a283ec599758a27ea95990ef7e798e8
DVL7 <derylodawid@gmail.com>
2026-04-30 03:29:15 +0200

    add: basic dbcontext

--- Infrastructure/Data/AppDbContext.cs
+++ Infrastructure/Data/AppDbContext.cs
@@ -1,11 +1,32 @@
 using Microsoft.EntityFrameworkCore;
+using FinStatsWallet.Domain.Entities;
 
 namespace FinStatsWallet.Infrastructure.Data;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options)
+    public DbSet<Account> Accounts => Set<Account>();
+    public DbSet<BudgetCategoryLimit> BudgetCategoryLimits => Set<BudgetCategoryLimit>();
+    public DbSet<BudgetPeriod> BudgetPeriods => Set<BudgetPeriod>();
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<Transaction> Transactions => Set<Transaction>();
+    public DbSet<User> Users => Set<User>();
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
+        
     }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+    // Przydatne do occuredAt, createdAt, updatedAt. Do nadpisania.
+    /* 
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return base.SaveChangesAsync(cancellationToken);
+    }
+    */
 }
